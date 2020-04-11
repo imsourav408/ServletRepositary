37622294b04dfbd194ed324f2a8771250bd7e510
DeveloperA <DevA@gmail.com>
2020-04-11 14:16:51 +0530

    TL recreated Test.java

---
+++ DemoProj1/src/com/nt/test/Test.java
@@ -0,0 +1,13 @@
+package com.nt.test;
+
+import com.nt.beans.Utility;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Utility u1=new Utility();
+		System.out.println(u1.getSysDate());
+
+	}
+
+}
