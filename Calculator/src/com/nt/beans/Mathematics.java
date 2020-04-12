0fba08af9506fc6291fe90ce7e3607fa9bba211f
SOURAV KUMAR NAYAK <SOURAV KUMAR NAYAK@LAPTOP-RRTA7TQ3>
2020-04-12 12:36:50 +0530

    all done

--- Calculator/src/com/nt/beans/Mathematics.java
+++ Calculator/src/com/nt/beans/Mathematics.java
@@ -1,5 +1,13 @@
 package com.nt.beans;
 
 public class Mathematics {
-
+	public int sum(int a,int b) {
+		return a+b;
+	}
+	public int sub(int a,int b) {
+		return a-b;
+	}
+	public int mul(int a,int b) {
+		return a*b;
+	}
 }
