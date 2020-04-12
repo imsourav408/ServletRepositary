261d93b0a0bc25acc0ea62e61a3046fc8a559df4
TeamLeader <TL@gmail.com>
2020-04-12 12:47:58 +0530

    Done Leader

--- Calculator/src/com/nt/test/Calculator.java
+++ Calculator/src/com/nt/test/Calculator.java
@@ -5,7 +5,8 @@ import com.nt.beans.Mathematics;
 public class Calculator {
  public static void main(String[] args) {
 	Mathematics m=new Mathematics();
-	System.out.println(m.sum(20,30));
-	System.out.println(m.sub(34, 22));
+	System.out.println("Addition: "+m.sum(20,30));
+	System.out.println("Substraction: "+m.sub(34, 22));
+	System.out.println("Multiplication Result is: "+m.mul(24, 21));
  }
 }
