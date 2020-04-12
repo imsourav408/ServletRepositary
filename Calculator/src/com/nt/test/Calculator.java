15a533e97e7e646d0f92d06c3c9c3dd19355229b
SOURAV KUMAR NAYAK <SOURAV KUMAR NAYAK@LAPTOP-RRTA7TQ3>
2020-04-12 12:40:54 +0530

    all done

--- Calculator/src/com/nt/test/Calculator.java
+++ Calculator/src/com/nt/test/Calculator.java
@@ -1,5 +1,11 @@
 package com.nt.test;
 
-public class Calculator {
+import com.nt.beans.Mathematics;
 
+public class Calculator {
+ public static void main(String[] args) {
+	Mathematics m=new Mathematics();
+	System.out.println(m.sum(20,30));
+	System.out.println(m.sub(34, 22));
+ }
 }
