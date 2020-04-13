e318fd429cb01fdf09941fd835386dd731fde177
TeamLeader <TL@gmail.com>
2020-04-13 11:43:30 +0530

    banktest

--- BankAccount/src/com/bank/test/MainBank.java
+++ BankAccount/src/com/bank/test/MainBank.java
@@ -1,10 +1,30 @@
 package com.bank.test;
 
+import java.util.Scanner;
+
+import com.bank.beans.Bank;
+
 public class MainBank {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=null;
+		sc=new Scanner(System.in);
+		String bankName=null;
+		Bank b=null;
+		System.out.println("Enter your Bank Name");
+		bankName=sc.nextLine();
+		try {
+			b=(Bank)Class.forName(bankName).newInstance();
+		}
+		catch(ClassNotFoundException cnf) {
+			System.out.println("Sorry!! Bank is not available");
+			return;
+		}
+		catch(Exception e) {
+			System.out.println("Internal Problem Occur");
+			e.printStackTrace();
+			return;
+		}
 	}
 
 }
